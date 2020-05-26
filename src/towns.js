72e758876c16005e4acc682a6c1a94d7eeece586
Vladimir <hy6ukk@gmail.com>
2020-05-26 22:58:22 +0600

    first commit

---
+++ src/towns.js
@@ -0,0 +1,110 @@
+/*
+ Страница должна предварительно загрузить список городов из
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ и отсортировать в алфавитном порядке.
+
+ При вводе в текстовое поле, под ним должен появляться список тех городов,
+ в названии которых, хотя бы частично, есть введенное значение.
+ Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+
+ Во время загрузки городов, на странице должна быть надпись "Загрузка..."
+ После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+
+ Разметку смотрите в файле towns-content.hbs
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+
+ *** Часть со звездочкой ***
+ Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+ то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
+ При клике на кнопку, процесс загрузки повторяется заново
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то добавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+
+/*
+ Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ */
+function loadTowns() {
+
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json', true);
+
+        xhr.addEventListener('load', () => {
+            if (xhr.status !== 200) {
+                let error = new Error(xhr.status + ': ' + xhr.statusText);
+
+                reject(error);
+            } else {
+                let xhrResponse = xhr.responseText;
+                let cities = JSON.parse(xhrResponse);
+
+                cities.sort((city1, city2) => {
+                    if (city1.name > city2.name) {
+                        return 1;
+                    } else if (city1.name < city2.name) {
+                        return -1;
+                    }
+
+                    return 0;
+
+                });
+
+                resolve(cities);
+            }
+        });
+
+        xhr.send();
+
+    })
+
+}
+
+/*
+ Функция должна проверять встречается ли подстрока chunk в строке full
+ Проверка должна происходить без учета регистра символов
+
+ Пример:
+   isMatching('Moscow', 'moscow') // true
+   isMatching('Moscow', 'mosc') // true
+   isMatching('Moscow', 'cow') // true
+   isMatching('Moscow', 'SCO') // true
+   isMatching('Moscow', 'Moscov') // false
+ */
+// строка chunk
+// подстрока full
+function isMatching(full, chunk) {
+    chunk.toLowerCase().indexOf(chunk.toLowerCase(), full)
+    'StRiNg'.toLowerCase().indexOf('STRING'.toLowerCase())
+}
+
+/* Блок с надписью "Загрузка" */
+const loadingBlock = homeworkContainer.querySelector('#loading-block');
+/* Блок с текстовым полем и результатом поиска */
+const filterBlock = homeworkContainer.querySelector('#filter-block');
+/* Текстовое поле для поиска по городам */
+const filterInput = homeworkContainer.querySelector('#filter-input');
+/* Блок с результатами поиска */
+const filterResult = homeworkContainer.querySelector('#filter-result');
+
+filterInput.addEventListener('keyup', function () {
+    // это обработчик нажатия кливиш в текстовом поле
+});
+
+export {
+    loadTowns,
+    isMatching
+};
